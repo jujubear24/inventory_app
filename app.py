0976fb6a8f91eeb1fef6a67c0cbfd9c7dfb81477
jujubear <julesbahanyi@gmail.com>
2025-03-12 23:02:03 -0400

    add[routes]: Import and register routes_blueprint into our app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from models import db
+from routes import routes_blueprint 
 
 app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'  # Database URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db.init_app(app)
+
+app.register_blueprint(routes_blueprint) # Register the blueprint
 
 if __name__ == '__main__':
     with app.app_context():
