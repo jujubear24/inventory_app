1f99cb178918889364e1af37acd52e7017b973d0
jujubear <julesbahanyi@gmail.com>
2025-04-03 20:55:44 -0400

    feat[Login]: Add login page template

--- app/__init__.py
+++ app/__init__.py
@@ -51,12 +51,15 @@ def create_app(config_name=None):
 
     
     # Register blueprints
-    from app.routes import main_bp, products_bp, reports_bp, auth_bp
+    from app.routes import main_bp, products_bp, reports_bp
+    from app.routes.auth import auth_bp
+    from app.routes.admin import admin_bp
     app.register_blueprint(main_bp)
     app.register_blueprint(products_bp)
     app.register_blueprint(reports_bp)
     app.register_blueprint(auth_bp)
-    
+    app.register_blueprint(admin_bp)
+
 
 
     
