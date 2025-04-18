d1968367a4743a4cebd8da9b869380cd081f84dd
jujubear <julesbahanyi@gmail.com>
2025-04-18 10:46:03 -0400

    refactor[models/oauth]: Refactor the oauth.py model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,7 @@
 from .db import db
 from .product import Product
+from .user import User
+from .oauth import OAuth
 
 # Export all models
-__all__ = ['db', 'Product']
+__all__ = ['db', 'Product', 'User', 'OAuth']
--- app/models/oauth.py
+++ app/models/oauth.py
@@ -1,8 +1,24 @@
+from .db import db  
+from .user import User 
 from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
-from flask_login import UserMixin # Assuming User already has this
+from sqlalchemy.orm import relationship
 
+# Define the OAuth model using the mixin provided by Flask-Dance
 class OAuth(OAuthConsumerMixin, db.Model):
-    # Define foreign key relationship to your User model
+    """
+    Stores OAuth credentials and links them to a User.
+    Inherits provider, provider_user_id, created_at, and token columns
+    from OAuthConsumerMixin.
+    """
+    __tablename__ = 'flask_dance_oauth' 
+    
+    # Foreign key to link this OAuth entry to a user in your database
     user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
-    # Define relationship back to the User model
-    user = db.relationship(User)
+
+    # Define the relationship back to the User model.
+    # This allows you to easily access the User object from an OAuth object (oauth_entry.user)
+    # and potentially access OAuth entries from a User object if you add a backref.
+    user = relationship(User) # backref='oauth_connections' could be added to User if needed
+
+    def __repr__(self):
+        return f'<OAuth provider={self.provider} user_id={self.user_id}>'
