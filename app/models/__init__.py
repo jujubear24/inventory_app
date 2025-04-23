e0a82c433b5ef61d01ad0ff51e2ccfc115240ca8
jujubear <julesbahanyi@gmail.com>
2025-04-22 23:04:06 -0400

    add[Permission]: Add permission to export in model init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,7 +3,8 @@ from .product import Product
 from .user import User
 from .oauth import OAuth
 from .role import Role
+from .permission import Permission
 
 # Export all models
-__all__ = ['db', 'Product', 'User', 'OAuth', 'Role']
+__all__ = ['db', 'Product', 'User', 'OAuth', 'Role', 'Permission']
 
